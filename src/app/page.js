b395e5606fc83c9a51f2893606b9f76dfaddbf16
Shadab Anwar <shadabanwar1810@gmail.com>
2024-08-27 20:34:27 +0530

    fix : Syntax

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Content from "./components/Content"
+import Content from "./components/Content";
 export default function Home() {
   return (
     <>
